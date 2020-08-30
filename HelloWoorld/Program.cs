57be41a64e5923e7488a072aa84b1cc2feb49446
emilgus02 <emil.gustafsson@elev.ga.ntig.se>
2020-08-30 20:47:35 +0200

    läxa1

---
+++ HelloWoorld/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace HelloWoorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string liv;
+            int nummer = 2;
+
+            Console.WriteLine("Skriv in ditt namn");
+            string namn = Console.ReadLine();
+            Console.WriteLine("Skriv in din ålder");
+            int alder = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Är du vid liv?");
+
+            for (int i = 2; i == nummer;)
+            {
+
+                Console.WriteLine(" skriv ja eller nej");
+                liv = Console.ReadLine();
+                if (liv == "ja" || liv == "nej")
+                {
+                    i = 3;
+                }
+                if (liv == "ja")
+                {
+                            Console.WriteLine("Du heter " + namn + ", du är " + alder + " år gammal och du är vid liv");
+                } else
+                {
+                    Console.WriteLine("Du heter " + namn + ", du är " + alder + " år gammal och du är inte vid liv");
+                }
+            }
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace HelloNiklas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Niklas!");
         }
     }
 }
