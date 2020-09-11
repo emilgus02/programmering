099f1c670bdd489d94188f3290218c42c46597fa
emilgus02 <emil.gustafsson@elev.ga.ntig.se>
2020-09-11 12:09:44 +0200

    akrl

--- AnotherHelloWorld/Program.cs
+++ AnotherHelloWorld/Program.cs
@@ -7,15 +7,28 @@ namespace AnotherHelloWorld
         static void Main(string[] args)
         {
 
-            string[] namn = { "Emil", "Niklas", "Linnn", "Niklas", "Malte" };
+            string[] namn = new string[5];
+
+            Console.WriteLine("Skriv ett namn");
+
+            for (int i = 0; i < namn.Length; i++)
+            {
+                namn[i] = Console.ReadLine();
+
+                Console.WriteLine("Skriv ett till namn");
+            }
 
             Array.Reverse(namn);
+
+            Console.WriteLine();
+
             for (int i = 0; i < namn.Length; i++)
             {
                 Console.WriteLine(namn[i]);
-            } 
+            }
 
+            }   
         }
     }
-}
+
 
